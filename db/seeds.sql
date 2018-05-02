fb0702da8fa78c2f7693f6d27c647660b12df753
JLafazan <jeffandcassy1@gmail.com>
2018-05-02 12:47:18 -0700

    Updates

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+-- Insert a set of records.
+INSERT INTO burgers (burger_name) VALUES ('Swiss Burger');
+INSERT INTO burgers (burger_name) VALUES ('Mushroom Burger');
+INSERT INTO burgers (burger_name) VALUES ('Ranch Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Burger", true);
